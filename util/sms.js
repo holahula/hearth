77243308ab1b98f00d44276bfc1b37944fce747d
Austin Jiang <austin_jiang123@hotmail.com>
2018-02-03 17:43:51 -0500

    updated sms.js
    lol

--- util/sms.js
+++ util/sms.js
@@ -1,9 +1,22 @@
-const lib = require('lib');
-
+const lib = require('lib')({
+  token: process.env.LIBRARY_TOKEN
+});
 
 
 const sms = {
-    //helper sms funtions (ie send sms)
-}
+    parseMessage: (message) => {
+      var i = indexOf(" ");
+      var result = [message.splice(0, i), message.slice(i+1)];
+      return result;
+    },
+    textOut: async (sender, receiver, message)=>{
+      let result = await lib.messagebird.tel.sms({
+        originator: sender,
+        recipient: receiver,
+        body: message
+      });
+      return result;
+    }
+};
 
 module.exports = sms;
