a9739e5ef3d8f6a3269b1fc55edc34f3469aced1
Ali Nikseresht <ali.nikseresht21@gmail.com>
2018-02-03 21:53:48 -0500

    working add listing endpoint

--- functions/add_listing.js
+++ functions/add_listing.js
@@ -1,12 +1,22 @@
 const lib = require('lib');
-const db = require('db')
+const md5 = require('md5');
+
+const db = require('../util/dbhelper');
 /**
 * A basic todo app
 * @param {number} id
-* @param {object} location
+* @param {string} type
 * @param {string} description
+* @param {string} locationString
 * @returns {string}
 */
-module.exports = async (id, location, description, context) => {
-  //
+module.exports = async (id, type, description, locationString, context) => {
+  let uuid = md5(id);
+  let locationObj = await lib.shun.directions.locate(locationString);
+  console.log(locationObj.address);
+  if (type != 'food' && type != 'shelter'){
+    throw "Unrecognized listing type";
+  }
+  await db.addListing(uuid, type, description, locationObj);
+  return "This is a link to a listing page";
 };
