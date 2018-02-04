77985c56002dc42fd7c8e46b00165b49736f1c79
Austin Jiang <austin_jiang123@hotmail.com>
2018-02-03 21:30:43 -0500

    Update handle_message.js

--- functions/handle_message.js
+++ functions/handle_message.js
@@ -1,6 +1,7 @@
 const lib = require('lib');
 const sms = require('../util/sms.js');
 const db = require('../util/dbhelper.js');
+const nClosest = 5;
 /**
  * @param {string} sender The phone number that sent the text to be handled
  * @param {string} receiver The StdLib phone number that received the SMS
@@ -11,7 +12,9 @@ const db = require('../util/dbhelper.js');
 
 module.exports = async (sender, receiver, message, createdDatetime, context) => {
     let parsedInput = sms.parseMessage(message);
+    //let identifier = sms.findType(parsedInput[0].toString());
     db.setUser(sender, parsedInput[0], parsedInput[1]);
+
     //console.log(parsedInput);
     let sentText = await sms.textOut(receiver, sender, parsedInput[0]);
     //console.log(sentText);
