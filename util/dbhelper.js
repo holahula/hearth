95510f8b242f7cc1b0ceb38b0976b856a15451f4
Ali Nikseresht <ali.nikseresht21@gmail.com>
2018-02-03 22:17:00 -0500

    Finished nearby listing get addresss

--- util/dbhelper.js
+++ util/dbhelper.js
@@ -10,17 +10,17 @@ const dbhelper = {
     return new Promise((resolve, reject) => {
       MongoClient.connect(dbUrl, async (err, db) => {
         if (err) reject(err);
-        let dbo = db.db("qhacks2018");
+        let dbo = db.db('qhacks2018');
         let locationObj = await lib.shun.directions.locate(locationString);
         let closestObj = await lib.shun.directions.closest(locationObj.address, await getAllListings(type), nClosest);
         console.log(closestObj);
-        var userObj = {
+        let userObj = {
           id: id,
           location: locationObj,
           type: type,
           closest: closestObj
         }
-        dbo.collection("users").insertOne(userObj, (err, res) =>{
+        dbo.collection('users').insertOne(userObj, (err, res) =>{
           if (err) reject(err);
           db.close();
           resolve(closestObj);
@@ -33,19 +33,36 @@ const dbhelper = {
     return new Promise((resolve, reject) => {
       MongoClient.connect(dbUrl, async (err, db) => {
         if (err) reject(err);
-        let dbo = db.db("qhacks2018");
-        var listingObj = {
+        let dbo = db.db('qhacks2018');
+        let listingObj = {
           uuid: uuid,
           type: type,
           description: description,
           location: locationObj
         }
         console.log(listingObj)
-        dbo.collection("listings").insertOne(listingObj, (err, res) =>{
+        dbo.collection('listings').insertOne(listingObj, (err, res) =>{
           if (err) reject(err);
           db.close();
-          resolve("Success!");
+          resolve('Success!');
         });
+      });
+    });
+  },
+  getNearbyAddress: (id, index) => {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(dbUrl, async (err, db) =>{
+        if (err) throw err;
+        let dbo = db.db('qhacks2018');
+        let search = await dbo.collection('users').find({id: id});
+        search = search.toArray()
+        db.close();
+        if (len(search) < 1){
+          reject("No request made.");
+        }else if (index > len(search.closest)){
+          reject("Index out of range");
+        }
+        resolve(search[0].closest[index-1]);
       });
     });
   }
@@ -77,8 +94,8 @@ function getAllListings(type){
   return new Promise((resolve, reject) => {
     MongoClient.connect(dbUrl, async (err, db) => {
       if (err) reject(err);
-      let dbo = db.db("qhacks2018");
-      let toReturn = await dbo.collection("listings").find({type: type}).toArray();
+      let dbo = db.db('qhacks2018');
+      let toReturn = await dbo.collection('listings').find({type: type}).toArray();
       console.log(toReturn);
       db.close();
       resolve(toReturn);
